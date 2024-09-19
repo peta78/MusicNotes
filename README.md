4fdb520adec31b00e403227b1f00de33f725a4f8
Peter Taraba <taraba.peter@mail.com>
2024-09-18 19:09:35 -0700

    hunter is insane and says we are going to 7th octave :). damn boy, you can fucking sing

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # Music Notes
 
+WAV Source: [Hunter Flynn - You deserve it all](https://www.youtube.com/watch?v=WjuqwcEsJDY)
+
+Singer Wikipedia page: [Hunter Flynn webpage](https://hunterflynn.com/)
+
+Singer's Youtube: [Hunter Flynn](https://www.youtube.com/@hunterflynn)
+
+Note: C7 (full voice, very clean, damn boy...)
+
+![spectrum](darinkab6.png)
+
 WAV Source: [Darinka Rolincova - Strasidla](https://youtu.be/G0r06nbJoQ8?si=8l0269eAmpx9y7QM&t=49)
 
 Singer Wikipedia page: [Dara Rolins](https://en.wikipedia.org/wiki/Dara_Rolins)
 
 Singer's Youtube: [Dara Rolins](https://www.youtube.com/channel/UCMKV1A4A7p090CREb5NVbug) and [Darina Rolincova](https://www.youtube.com/@darinarolincova7719)
 
-Note: B6 (full voice, no falsetto, nor "cat in heat sound", roughly one second length) 
+Note: B6 (full voice, no falsetto, nor "cat in heat sound", roughly one second length)
 
 ![spectrum](darinkab6.png)
 
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from pydub import AudioSegment
 from pydub.utils import get_array_type
 
-sound = AudioSegment.from_file(file='mariah.wav')
+sound = AudioSegment.from_file(file='hunter.wav')
 left = sound.split_to_mono()[0]
 
 bit_depth = left.sample_width * 8
@@ -18,10 +18,13 @@ print(sound)
 print(sound.frame_rate, sound.duration_seconds, l.shape[0] / sound.frame_rate)
 
 # https://pages.mtu.edu/~suits/notefreqs.html
-notes = ['C5', 'C#5/Db5', 'D5', 'D#5/Eb5', 'E5', 'F5', 'F#5/Gb5', 'G5', 'G#5/Ab5', 'A5', 'A#5/Bb5', 'B5',
-    'C6', 'C#6/Db6', 'D6', 'D#6/Eb6', 'E6', 'F6', 'F#6/Gb6', 'G6', 'G#6/Ab6', 'A6', 'A#6/Bb6', 'B6']
-noteFrequencies = [523.25, 554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61, 880, 932.33, 987.77,
-    1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760, 1864.66, 1975.53]
+notes = [
+    'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6', 'A6', 'A#6', 'B6',
+    'C7', 'C#7', 'D7', 'D#7', 'E7', 'F7', 'F#7', 'G7', 'G#7', 'A7', 'A#7', 'B7']
+noteFrequencies = [
+    1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760, 1864.66, 1975.53,
+    2093.005, 2217.461, 2349.318, 2489.016, 2637.021, 2793.826, 2959.956, 3135.964, 3322.438, 3520.000, 3729.310, 3951.066,
+    ]
 
 closestFreq = [float('inf') for i in range(len(notes))]
 closestInd = [-1 for i in range(len(notes))]
